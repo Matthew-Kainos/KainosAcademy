6f612ef9c8a316142d5cd36e521a9bf822ccca58
Gareth Nelson <gareth.nelson@kainos.com>
2021-07-06 15:24:27 +0100

    backend gets info from db

--- model/dbCommands.js
+++ model/dbCommands.js
@@ -1,25 +1,26 @@
-// const mysql = require('mysql'); 
-// const dbconfig = require('../dbconfig.json'); 
-// const util = require ('util');
+const mysql = require('mysql'); 
+const dbconfig = require('../dbconfig.json'); 
+const util = require ('util');
 
-// function wrapDB (dbconfig) { 
-//     const pool = mysql.createPool(dbconfig) 
-//     return { 
-//         query(sql, args) { 
-//             console.log("in query in wrapper") 
-//             return util.promisify( pool.query ) 
-//             .call(pool, sql, args) 
-//         }, 
-//         release () { 
-//             return util.promisify( pool.releaseConnection ) 
-//             .call( pool ) 
-//         } 
-//     } 
-// }
+function wrapDB (dbconfig) { 
+    const pool = mysql.createPool(dbconfig) 
+    return { 
+        query(sql, args) { 
+            console.log("in query in wrapper") 
+            return util.promisify( pool.query ) 
+            .call(pool, sql, args) 
+        }, 
+        release () { 
+            return util.promisify( pool.releaseConnection ) 
+            .call( pool ) 
+        } 
+    } 
+}
 
-// exports.getEmployees = async () => { 
-//     return await db.query( 
-//         "SELECT * FROM Employee");
-// }
+exports.getRolesByBand = async (id) => { 
+    return await db.query( 
+        "SELECT * FROM JobRoles JOIN Band ON JobRoles.Band_ID "
+        + "= Band.Band_ID WHERE JobRoles.Band_ID = ?", id); 
+}
 
-// const db = wrapDB(dbconfig);
+const db = wrapDB(dbconfig);
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,10 +1,17 @@
 var express = require('express')
 var router = express.Router()
+const dbCommands = require('../model/dbCommands');
 
-
-router.get('/hello', function (req, res) {
-    res.status(200);
-    res.send('Hello');
+router.get('/band/:substr', async function (req, res) {
+    try{
+        var id = parseInt(req.params.substr);
+        const results = await dbCommands.getRolesByBand(id);
+        res.send(results);
+        res.status(200);
+    } catch(e){
+        res.status(500);
+        res.send('Error');
+    }
 })
   
 module.exports = router
