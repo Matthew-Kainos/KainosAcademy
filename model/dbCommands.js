7ac99f010c5462bd7459ac4ef9afc842f382a1ff
AnnaB <anna.boryniec@kainos.com>
2021-07-06 19:22:06 +0200

    added backend - show all job roles

--- model/dbCommands.js
+++ model/dbCommands.js
@@ -1,25 +1,25 @@
-// const mysql = require('mysql'); 
-// const dbconfig = require('../dbconfig.json'); 
-// const util = require ('util');
+const mysql = require('mysql'); 
+const dbconfig = require('../dbconfig.json'); 
+const util = require ('util');
 
-// function wrapDB (dbconfig) { 
-//     const pool = mysql.createPool(dbconfig) 
-//     return { 
-//         query(sql, args) { 
-//             console.log("in query in wrapper") 
-//             return util.promisify( pool.query ) 
-//             .call(pool, sql, args) 
-//         }, 
-//         release () { 
-//             return util.promisify( pool.releaseConnection ) 
-//             .call( pool ) 
-//         } 
-//     } 
-// }
+function wrapDB (dbconfig) { 
+    const pool = mysql.createPool(dbconfig) 
+    return { 
+        query(sql, args) { 
+            console.log("in query in wrapper") 
+            return util.promisify( pool.query ) 
+            .call(pool, sql, args) 
+        }, 
+        release () { 
+            return util.promisify( pool.releaseConnection ) 
+            .call( pool ) 
+        } 
+    } 
+}
 
-// exports.getEmployees = async () => { 
-//     return await db.query( 
-//         "SELECT * FROM Employee");
-// }
+exports.getJobRoles = async () => { 
+    return await db.query( 
+        "SELECT Role_ID, Name FROM JobRoles");
+}
 
-// const db = wrapDB(dbconfig);
+const db = wrapDB(dbconfig);
--- routes/jobs.js
+++ routes/jobs.js
@@ -1,3 +1,4 @@
+const dbCommands = require('../model/dbCommands');
 var express = require('express')
 var router = express.Router()
 
@@ -6,6 +7,10 @@ router.get('/hello', function (req, res) {
     res.status(200);
     res.send('Hello');
 })
+
+router.get('/job-roles', async (req, res) => 
+{ const dbResults = await dbCommands.getJobRoles(); 
+    res.send(dbResults);});
   
 module.exports = router
   
