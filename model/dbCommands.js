f079ad711cb5e57bd1428840943c398ddcc99883
Matthew <matthew.mccaig@kainos.com>
2021-07-08 14:28:14 +0100

    Added integration tests for getJobSpec

--- model/dbCommands.js
+++ model/dbCommands.js
@@ -18,6 +18,35 @@ function wrapDB (dbconfig) {
     } 
 }
 
+const db = wrapDB(dbconfig);
+
+exports.getCapabilitiesBasedOnJobId = async (jobId) => { 
+    try{
+        return await db.query( 
+            "SELECT Capabilities.cap_id, Capabilities.name FROM Capabilities LEFT JOIN JobRoles ON Capabilities.cap_id = JobRoles.cap_id WHERE JobRoles.role_id = ? LIMIT 1;", jobId);
+    } catch(e) {
+        throw new DatabaseError(`Error calling getCapabilitiesBasedOnJobId with message: ${e.message}`);
+    }
+}
+
+exports.checkIfJobIdExists = async (jobId) => { 
+    try{
+        return await db.query( 
+            "SELECT * FROM JobRoles WHERE ROLE_ID = ? LIMIT 10;", jobId);
+        } catch(e) {
+        throw new DatabaseError(`Error calling checkIfJobIdExists with message: ${e.message}`);
+    }
+}
+
+exports.testInsertCapability = async (jobId) => { 
+    try{
+        return await db.query( 
+            'INSERT INTO Capabilities values (90000, "TestName", "TestJobFamily","TestLeadName", "TestLeadMessage", 2)');
+        } catch(e) {
+        throw new DatabaseError(`Error calling testInsertCapability with message: ${e.message}`);
+    }
+}
+
 exports.getEmployees = async () => { 
     return await db.query( 
         "SELECT * FROM Employee");
@@ -35,4 +64,3 @@ exports.getJobSpec = async (Role_ID) => {
     
  }
 
-const db = wrapDB(dbconfig);
