32aba819702a458178c5db43041aa6c5013628d6
Matthew <matthew.mccaig@kainos.com>
2021-07-09 11:39:06 +0100

    Added error handling per PR comment

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,11 +7,24 @@ router.get('/hello', function (req, res) {
     res.send('Hello');
 })
 
-router.get('/job-roles-spec/:Role_ID', async(req, res) =>
-{   
-    const Role_ID = req.params.Role_ID;
-    const dbResults = await dbCommands.getJobSpec(Role_ID); res.send(dbResults);
-});
+router.get('/job-roles-spec/:Role_ID', async function (req, res) {
+    try{
+        const Role_ID = req.params.Role_ID;
+        const dbResults = await dbCommands.getJobSpec(Role_ID); 
+        res.send(dbResults);
+        res.status(200);
+    } catch(e){
+        res.status(500);
+        if(e instanceof DatabaseError) {
+            res.send('Database Error');
+            console.error(e.message);
+        }
+        res.send('Error');
+        console.error(e.message);
+    }
+})
+
+
   
 module.exports = router
   
