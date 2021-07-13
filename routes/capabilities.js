b550ec1e806c09ffea115aab9e2eaf51a8b2a82d
Triona <triona.hughes@kainos.com>
2021-07-13 12:02:12 +0100

    adding updated files to branch

--- routes/capabilities.js
+++ routes/capabilities.js
@@ -1,26 +1,56 @@
-const express = require('express')
+const express = require('express');
+
 const router = express.Router();
 const dbCommands = require('../model/dbCommands');
+const DatabaseError = require('../errors/DatabaseError');
 
-
-router.get('/', function (req, res) {
+router.get('/findByJobId/:id', async (req, res) => {
+  try {
+    const jobId = req.params.id;
+    const results = await dbCommands.getCapabilitiesBasedOnJobId(jobId);
+    res.send(results);
     res.status(200);
-    res.send('Hello')
-})
-
-router.get('/viewCapabilityLead/:capID', async function (req, res){
-    try{
-        const capID = req.params.capID;
-        const results = await dbCommands.getCapabilityLead(capID);
-        console.log(results);
-        res.send(results);
-        res.status(200);
-    } catch(e){
-        res.status(500);
-        res.send('Error');
-        console.error(e.message);
+  } catch (e) {
+    res.status(500);
+    if (e instanceof DatabaseError) {
+      res.send('Database Error');
+      console.error(e.message);
     }
-})
-  
-module.exports = router
-  
+    res.send('Error');
+    console.error(e.message);
+  }
+});
+
+router.get('/findByJobName/:jobName', async (req, res) => {
+  try {
+    const name = req.params.jobName;
+    const results = await dbCommands.getCapabilitiesBasedOnJobName(`%${name}%`);
+    res.send(results);
+    res.status(200);
+    res.send('Hello');
+  } catch (e) {
+    res.status(500);
+    if (e instanceof DatabaseError) {
+      res.send('Database Error');
+      console.error(e.message);
+    }
+    res.send('Error');
+    console.error(e.message);
+  }
+});
+
+router.get('/viewCapabilityLead/:capID', async (req, res) => {
+  try {
+    const { capID } = req.params;
+    const results = await dbCommands.getCapabilityLead(capID);
+    console.log(results);
+    res.send(results);
+    res.status(200);
+  } catch (e) {
+    res.status(500);
+    res.send('Error');
+    console.error(e.message);
+  }
+});
+
+module.exports = router;
